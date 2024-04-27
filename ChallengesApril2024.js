3e8546ed5572959bb6f3cd915750e7d1952e953c
longhairshortcode <116141725+longhairshortcode@users.noreply.github.com>
2024-04-26 23:39:39 -0700

    Update ChallengesApril2024.js

--- ChallengesApril2024.js
+++ ChallengesApril2024.js
@@ -1,6 +1,8 @@
 
         function sum (numbers) {
     if (numbers.length === 0){
+        function sum (numbers) {
+    if (numbers.length === 0){
 
         function sum (numbers) {
     if (numbers.length === 0){
