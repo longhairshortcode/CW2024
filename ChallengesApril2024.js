a37597c548f41de66fa2a70564ca455aefe3afe7
longhairshortcode <116141725+longhairshortcode@users.noreply.github.com>
2024-05-01 17:06:49 -0700

    Update ChallengesApril2024.js

--- ChallengesApril2024.js
+++ ChallengesApril2024.js
@@ -2,7 +2,6 @@
 
   
 
- 0){if (numbers.length === 0){if (numbers.length === 0){if (numbers.length === 0){if (numbers.length === 0){if (numbers.length === 0){
 
     if (numbers.length === 0){if (numbers.
     if (numbers.length === 0){if (numbers.length === 0){
