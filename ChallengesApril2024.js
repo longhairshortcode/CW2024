ccd664213ce9bef71eaacb8d306b3a63ea1454df
longhairshortcode <116141725+longhairshortcode@users.noreply.github.com>
2024-05-16 17:37:06 -0700

    Update ChallengesApril2024.js

--- ChallengesApril2024.js
+++ ChallengesApril2024.js
@@ -1,28 +1,4 @@
 
-
-        function sum (numbers) {
-        function sum (numbers) {function sum (numbers) {
-
-        function sum (numbers) {
-        function sum (numbers) {
-        function sum (numbers) {
-        function sum (numbers) {
-        function sum (numbers) {
-        function sum (numbers) {
-  
-        function sum (numbers) {      function sum (numbers) {
-
-
-
-        function sum (numbers) {
-    if (numbers.length === 0){if (numbers.length === 0){
-function sum (numbers) {
-    if (numbers.length === 0){if (numbers.length === 0){
-
-
-
-//Tuesday March 26, 2024
-
 //TASK*******
 
 //Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
