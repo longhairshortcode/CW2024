2d33c13a57bee245776452eca30df58815e311e9
longhairshortcode <116141725+longhairshortcode@users.noreply.github.com>
2024-05-22 23:52:02 -0700

    Update ChallengesApril2024.js

--- ChallengesApril2024.js
+++ ChallengesApril2024.js
@@ -1,4 +1,9 @@
-
+function sum (numbers) {
+    if (numbers.length === 0){
+      return 0
+    }else
+  return numbers.reduce((a, b) => a + b)
+};
 //TASK*******
 
 //Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
